514e32fc157366f40c6eafb7d817b6ce6dc4ddbe
Slawek <yarpiin@gmail.com>
2016-12-10 11:49:18 +0000

    add Bioshock gov
    # Conflicts:
    #	arch/arm64/configs/radioactive_defconfig
    #	drivers/cpufreq/Makefile
    #	include/linux/cpufreq.h
    
    # Conflicts:
    #	arch/arm64/configs/radioactive_defconfig
    
    # Conflicts:
    #	drivers/cpufreq/Makefile
    #	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -548,6 +548,9 @@ extern struct cpufreq_governor cpufreq_gov_nebula;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
 extern struct cpufreq_governor cpufreq_gov_chill;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_chill)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
 #endif
 
 /*********************************************************************
