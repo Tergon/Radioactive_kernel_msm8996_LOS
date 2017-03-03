21e4ad253a8126b42290802739c49ed087771cab
flar2 <asegaert@gmail.com>
2017-03-02 23:18:02 -0500

    cpufreq: cache tunables for ondemand and conservative governors
    This makes them work better with big.LITTLE setups.  Previously, all big cluster tunables were lost when a cluster went offline.
    
    # Conflicts:
    #	drivers/cpufreq/cpufreq_governor.c

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -287,8 +287,8 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			rc = cdata->init_ex(dbs_data, policy);
 		} else if (cdata->governor == GOV_ZZMOOVE) {
 			rc = cdata->init_zz(dbs_data, policy);
-		} else
-			rc = cdata->init(dbs_data);
+		else
+			rc = cdata->init(dbs_data, policy);
 
 		if (rc) {
 			pr_err("%s: POLICY_INIT: init() failed\n", __func__);
