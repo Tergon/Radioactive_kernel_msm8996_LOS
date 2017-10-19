d74966a226fd35616f7a94863c23709c33569fc8
acuicultor <ramoncuenca4@gmail.com>
2017-10-19 16:45:08 +0200

    Revert "kernel/power/powersuspend: new PM kernel driver for Android w/o early suspend v1.5 (faux123/Yank555.lu)"
    This reverts commit 82fb2c9c4aff69e8bbb511e2bf14683380b353cc.
    
    # Conflicts:
    #	drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -27,10 +27,6 @@
 #include "mdss_dba_utils.h"
 #include "mdss_livedisplay.h"
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#endif
-
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -784,10 +780,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
-
-#ifdef CONFIG_POWERSUSPEND
-	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
-#endif
 	
 	display_on = true;
 
@@ -896,10 +888,6 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dba_utils_video_off(pinfo->dba_data);
 		mdss_dba_utils_hdcp_enable(pinfo->dba_data, false);
 	}
-    
-#ifdef CONFIG_POWERSUSPEND
-	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
-#endif
 
 	display_on = false;
 
