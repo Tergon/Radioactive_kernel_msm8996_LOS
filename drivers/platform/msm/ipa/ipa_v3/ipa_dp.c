PhenomX1998/kernel_oneplus_msm8996

Fix standalone compiling

drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -17,7 +17,7 @@
 #include <linux/netdevice.h>
 #include <linux/msm_gsi.h>
 #include "ipa_i.h"
-#include "ipa_trace.h"
+#include <trace/ipa_trace_v3.h>
 
 #define IPA_LAST_DESC_CNT 0xFFFF
 #define POLLING_INACTIVITY_RX 40

Tergon/Radioactive_kernel_msm8996_LOS

Signed-off-by: RenderBroken <zkennedy87@gmail.com>

drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -17,7 +17,7 @@
 #include <linux/netdevice.h>
 #include <linux/msm_gsi.h>
 #include "ipa_i.h"
-#include "ipa_trace.h"
+#include <trace/ipa_trace.h>
 #include "ipahal/ipahal.h"
 
 #define IPA_LAST_DESC_CNT 0xFFFF